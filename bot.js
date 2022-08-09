6060bb5dcd0e7731c02094a2e7f0b11cda955973
Glitch (judicious-fluorescent-dreadnought) <none>
2022-08-09 09:01:48 +0000

    📻🏀 Checkpoint ./bot.js:41138121/4173

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const db = require('quick.db')
 const moment = require("moment");
 const ayarlar=require("./ayarlar.json");//Rache Code , Telif Hakları Vardır, https://discord.gg/VcfpUgtajB//Rache Code , Telif Hakları Vardır, https://discord.gg/VcfpUgtajB//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5
 const express = require('express');
+require('discord-buttons')(client);
 ///////Rache Code , Telif Hakları Vardır, https://discord.gg/VcfpUgtajB//Rache Code , Telif Hakları Vardır, https://discord.gg/VcfpUgtajB//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5
 const app = express()//Rache Code , Telif Hakları Vardır, https://discord.gg/VcfpUgtajB//Rache Code , Telif Hakları Vardır, https://discord.gg/VcfpUgtajB//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5
 app.get('/', (req, res) => res.send("Bot Aktif"))
@@ -948,3 +949,79 @@ client.ekoayarlar = {//Rache Code , Telif Hakları Vardır, https://discord.gg/m
   admin: ["666223000739381258"]//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5
 }//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5//Rache Code , Telif Hakları Vardır, https://discord.gg/mMMpTt5
 
+const disbut = require('discord-buttons');
+client.on('clickButton', async (button) => {
+
+  const onybuton = new disbut.MessageButton()
+    .setLabel('Onaylandı')
+    .setStyle('green')
+    .setID('ony')
+    .setDisabled();
+
+    const onaymsj = new Discord.MessageEmbed()
+    .setAuthor('RabeL #Club', button.message.guild.iconURL({dynamic: true, type: 'gif', size: 1024}))
+    .setDescription(`Başvurunuz Onaylandı ve Yetkili Rolleriniz verildi, Tebrikler :)`)
+    .setColor('GREEN');
+
+
+
+    const data = await db.get(`basvur.${button.message.id}`)
+    if(!data) return;
+  const basvuruGonderenID = data;
+
+  if(button.id === 'onay'){
+    button.reply.defer()
+	const isimdes = client.users.cache.get(basvuruGonderenID);
+    await button.message.edit(`<@${basvuruGonderenID}> adlı kişinin, Başvurusu \`${button.clicker.user.tag}\` isimli yetkili tarafından Kabul edildi`, onybuton)
+    await client.channels.cache.get(ayarlar.onayred).send(`<@${basvuruGonderenID}>,`, onaymsj)
+    await client.guilds.cache.get(ayarlar.sunucuid).members.cache.get(basvuruGonderenID).roles.add(ayarlar.yetkilirolid)
+	isimdes.send('Hey Selam! Ben RabeL :wave: \nYaptığın yetkili başvurusu onaylandı öncelikle tebrik ederim artık yetkili ekibimizdensin. :partying_face: \nAncak bazı görevlerin olucak alta bunları anlatımcam iyi dinle olur mu :slight_smile: \n\n\n **1 -** <#874742022903308338> Kanalında Aktif Bir Şekilde Çalışmak <#874742017333264595> Bir Kez Daha Okumanı Tavsiye Ederim.\nAbone Rol Verme Komutu = !a <@kullanıcı>\nEğer Yanlışıkla Verirsen !a-al <@kullanıcı>\n\n**2 -** <#874742026762080306> Bildiğin Hatalar Varsa Yardım Edebilirsin.\n\n**3 -** <#755298820182638692> Kanalını Oku Kurala Bile Uymadığın Zaman sende üyeler gibi ceza yiyebilirsin.\n\n**4 -** <#874743904799449108> Kanalınıda Okumayı Tavsiye Ederiz Aynı Şekilde Uymassan Yetkin Gidebilir vb.\n\n**5 **- <#874749694906093578> Her Etiket Geldiğinde Bakman Senin İçin İyi Olabilir Arada Toplantılar Olabilir Eğer Zorunlu olan Toplantılara Gelmezsen Destek Ekibinden Atılırsın.\n\n\n **Evet ama hep böyle sıkı yönetim mi var hep çalışmak mı var?**\nTabikide hayır. Arasıra yetkili ekibimiz arasında oynadığımız eğlenceli vakitlerde oluyor, birlikte oyunlar oynar şakalaşırız. Hatta çoğu yetkililerimiz  videolarda bile çıktı :) \n\n **Gelelim Ektiğimizi Biçmeye**\nAktif ve Düzenli Çalışmanın ardından tabikide ödüller var eğer kendini gösterirsen sırasıyla yetkin yükselicek ve daha üst konumlarda görev alıcaksın. \n O zaman Şimdiden kolay gelsin :) RabeL Yetkili Ekibine Hoş Geldin :heart:')
+  }
+  if(button.id === 'red'){
+    button.reply.defer()
+
+
+    const sorular = [
+      '**Reddedilme Sebebi?** <cevap vermek için 3 dakikan var>'
+    ]
+    let sayac = 0
+    
+    const filter = m => m.author.id === button.clicker.user.id
+    const collector = new Discord.MessageCollector(button.channel, filter, {
+      max: sorular.length,
+      time: 3000 * 60
+    })
+
+    button.channel.send(sorular[sayac++])
+    collector.on('collect', m => {
+      if(sayac < sorular.length){
+        m.channel.send(sorular[sayac++])
+      }
+    })
+
+
+    collector.on('end', collected => {
+      if(!collected.size) return button.channel.send('**Süre Bitti!**');
+      button.channel.send('**Başvurunuz Başarıyla Reddedildi.**');
+
+           
+    const redbuton = new disbut.MessageButton()
+    .setLabel('Reddedildi')
+    .setStyle('red')
+    .setID('red')
+    .setDisabled();
+
+    const redmsg = new Discord.MessageEmbed()
+    .setAuthor('RabeL #Club', button.message.guild.iconURL({dynamic: true, type: 'gif', size: 1024}))
+    .setDescription(`<@${basvuruGonderenID}> Başvurunuz, \`${collected.map(m => m.content).slice(0,1)}\` nedeniyle ${button.clicker.user} tarafından Reddedildi`)
+    .setColor('RED');
+
+     button.message.edit(`<@${basvuruGonderenID}> adlı kişinin, Başvurusu, \`${collected.map(m => m.content).slice(0,1)}\` Sebebiyle, \`${button.clicker.user.tag}\` isimli yetkili tarafından Başarıyla Reddedildi`, redbuton)
+     client.channels.cache.get(ayarlar.onayred).send(`<@${basvuruGonderenID}>,`, redmsg)
+          })
+
+    
+  }
+  db.delete(`basvuru.${button.message.id}`)
+
+});
